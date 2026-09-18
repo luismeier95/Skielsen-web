32088a35128e8da3cef8d56a18478944d5400f26
luismeier95 <luis.meier95@googlemail.com>
2026-09-18 12:13:41 +0200

    Set SKIELSEN version to V15.1.9

--- public/assets/js/00-version.js
+++ public/assets/js/00-version.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const VERSION='15.1.8';
+const VERSION='15.1.9';
 window.SKIELSEN_VERSION=VERSION;
 function syncVersion(root=document){
   document.title=`SKIELSEN V${VERSION}`;
