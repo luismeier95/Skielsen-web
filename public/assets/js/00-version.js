d2a76c2cea883ed8e8b351658e0ad21cf828b591
luismeier95 <luis.meier95@googlemail.com>
2026-09-19 15:28:52 +0200

    Restore validator-compatible Core canvas rule V15.1.45

--- public/assets/js/00-version.js
+++ public/assets/js/00-version.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const VERSION='15.1.44';
+const VERSION='15.1.45';
 window.SKIELSEN_VERSION=VERSION;
 function syncVersion(root=document){
   document.title=`SKIELSEN V${VERSION}`;
