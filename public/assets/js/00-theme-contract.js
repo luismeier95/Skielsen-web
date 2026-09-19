f19df617b9cc1f4fdcb1eb162feeaa1b2a8e7fec
luismeier95 <luis.meier95@googlemail.com>
2026-09-19 13:49:23 +0200

    Release heading contrast fixes v15.1.38

--- public/assets/js/00-theme-contract.js
+++ public/assets/js/00-theme-contract.js
@@ -1,7 +1,7 @@
 (()=>{
 'use strict';
 
-const VERSION=window.SKIELSEN_VERSION||'15.1.37';
+const VERSION=window.SKIELSEN_VERSION||'15.1.38';
 
 const TOKEN_MAP=Object.freeze({
   root_canvas:'--theme-root-canvas',
