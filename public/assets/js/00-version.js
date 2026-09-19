d370e0a8b654de99319c6d5769185847ec72a8bb
luismeier95 <luis.meier95@googlemail.com>
2026-09-19 13:59:12 +0200

    Release MATCHES theme contract fixes v15.1.39

--- public/assets/js/00-version.js
+++ public/assets/js/00-version.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const VERSION='15.1.38';
+const VERSION='15.1.39';
 window.SKIELSEN_VERSION=VERSION;
 function syncVersion(root=document){
   document.title=`SKIELSEN V${VERSION}`;
