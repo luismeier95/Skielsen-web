79f16acd9310a4238c4b4fd2bed8fbd6c3358f3c
luismeier95 <luis.meier95@googlemail.com>
2026-09-19 13:37:34 +0200

    Sync SKIELSEN version fallbacks to 15.1.36

--- public/assets/js/00-theme-contract.js
+++ public/assets/js/00-theme-contract.js
@@ -1,7 +1,7 @@
 (()=>{
 'use strict';
 
-const VERSION=window.SKIELSEN_VERSION||'15.1.35';
+const VERSION=window.SKIELSEN_VERSION||'15.1.36';
 
 const TOKEN_MAP=Object.freeze({
   root_canvas:'--theme-root-canvas',
