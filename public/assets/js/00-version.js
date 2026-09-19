5a6a09a22c84a84822dafd13857c5f6486ea990e
luismeier95 <luis.meier95@googlemail.com>
2026-09-19 13:36:52 +0200

    Release non-Core Theme Contract fixes v15.1.36

--- public/assets/js/00-version.js
+++ public/assets/js/00-version.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const VERSION='15.1.35';
+const VERSION='15.1.36';
 window.SKIELSEN_VERSION=VERSION;
 function syncVersion(root=document){
   document.title=`SKIELSEN V${VERSION}`;
