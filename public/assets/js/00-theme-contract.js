c3dc064de8faa726666b05dbd852210ba1343a2f
luismeier95 <luis.meier95@googlemail.com>
2026-09-19 15:04:54 +0200

    Standardize all main page headers to MATCHES style V15.1.42

--- public/assets/js/00-theme-contract.js
+++ public/assets/js/00-theme-contract.js
@@ -1,7 +1,7 @@
 (()=>{
 'use strict';
 
-const VERSION=window.SKIELSEN_VERSION||'15.1.41';
+const VERSION=window.SKIELSEN_VERSION||'15.1.42';
 
 const TOKEN_MAP=Object.freeze({
   root_canvas:'--theme-root-canvas',
--- public/assets/js/00-version.js
+++ public/assets/js/00-version.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const VERSION='15.1.41';
+const VERSION='15.1.42';
 window.SKIELSEN_VERSION=VERSION;
 function syncVersion(root=document){
   document.title=`SKIELSEN V${VERSION}`;
