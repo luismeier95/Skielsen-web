748d2649a5a4cdd67f5796e933c9d6452710e876
luismeier95 <luis.meier95@googlemail.com>
2026-09-19 10:34:58 +0200

    Update 00-version.js for V15.1.27

--- public/assets/js/00-version.js
+++ public/assets/js/00-version.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const VERSION='15.1.26';
+const VERSION='15.1.27';
 window.SKIELSEN_VERSION=VERSION;
 function syncVersion(root=document){
   document.title=`SKIELSEN V${VERSION}`;
