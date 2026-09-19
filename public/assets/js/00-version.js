580db6c834aaf783c281ec517700550dc53ecf5b
luismeier95 <luis.meier95@googlemail.com>
2026-09-19 11:24:23 +0200

    Bump SKIELSEN cache version to 15.1.30

--- public/assets/js/00-version.js
+++ public/assets/js/00-version.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const VERSION='15.1.29';
+const VERSION='15.1.30';
 window.SKIELSEN_VERSION=VERSION;
 function syncVersion(root=document){
   document.title=`SKIELSEN V${VERSION}`;
