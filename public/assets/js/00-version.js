80a54fe386d2ec9bb2bc0272c9bcdf9211f9c7d1
luismeier95 <luis.meier95@googlemail.com>
2026-09-19 12:22:50 +0200

    Finalize SKIELSEN Core 2 release v15.1.34

--- public/assets/js/00-version.js
+++ public/assets/js/00-version.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const VERSION='15.1.33';
+const VERSION='15.1.34';
 window.SKIELSEN_VERSION=VERSION;
 function syncVersion(root=document){
   document.title=`SKIELSEN V${VERSION}`;
