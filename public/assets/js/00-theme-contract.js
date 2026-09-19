4526ea9ea9111a0e67c2376215af8608044f9f6f
luismeier95 <luis.meier95@googlemail.com>
2026-09-19 13:08:51 +0200

    Finalize Core/Core 2 split v15.1.35

--- public/assets/js/00-theme-contract.js
+++ public/assets/js/00-theme-contract.js
@@ -1,7 +1,7 @@
 (()=>{
 'use strict';
 
-const VERSION=window.SKIELSEN_VERSION||'15.1.34';
+const VERSION=window.SKIELSEN_VERSION||'15.1.35';
 
 const TOKEN_MAP=Object.freeze({
   root_canvas:'--theme-root-canvas',
