6dbadd23abe4d34ae18032c17019e26250cd6bef
luismeier95 <luis.meier95@googlemail.com>
2026-09-19 15:14:43 +0200

    Fix Core themes in shop and Core 2 color strip V15.1.43

--- public/assets/js/00-version.js
+++ public/assets/js/00-version.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const VERSION='15.1.42';
+const VERSION='15.1.43';
 window.SKIELSEN_VERSION=VERSION;
 function syncVersion(root=document){
   document.title=`SKIELSEN V${VERSION}`;
