c20fd9c6f939e3f0c31ff3f2b660a4acc5510408
luismeier95 <luis.meier95@googlemail.com>
2026-09-19 15:27:52 +0200

    Unify Core general accents to purple across all pages V15.1.44

--- public/assets/js/00-version.js
+++ public/assets/js/00-version.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const VERSION='15.1.43';
+const VERSION='15.1.44';
 window.SKIELSEN_VERSION=VERSION;
 function syncVersion(root=document){
   document.title=`SKIELSEN V${VERSION}`;
