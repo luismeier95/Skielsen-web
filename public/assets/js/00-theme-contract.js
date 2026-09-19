fc5f6ed06a018a724bcdda259f534a1f05830dc4
luismeier95 <luis.meier95@googlemail.com>
2026-09-19 14:07:43 +0200

    Release incremental bracket propagation v15.1.40

--- public/assets/js/00-theme-contract.js
+++ public/assets/js/00-theme-contract.js
@@ -1,7 +1,7 @@
 (()=>{
 'use strict';
 
-const VERSION=window.SKIELSEN_VERSION||'15.1.39';
+const VERSION=window.SKIELSEN_VERSION||'15.1.40';
 
 const TOKEN_MAP=Object.freeze({
   root_canvas:'--theme-root-canvas',
