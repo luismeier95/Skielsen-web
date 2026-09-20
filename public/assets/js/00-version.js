f3c77bdffc1902d8071091911a67a2d62773b255
luismeier95 <luis.meier95@googlemail.com>
2026-09-21 01:04:45 +0200

    Fix mobile admin command dock and tournament title

--- public/assets/js/00-version.js
+++ public/assets/js/00-version.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const VERSION='15.1.72';
+const VERSION='15.1.73';
 window.SKIELSEN_VERSION=VERSION;
 function syncVersion(root=document){
   document.title=`SKIELSEN V${VERSION}`;
