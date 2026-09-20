e7b5cc4502629184d44193f15ce4aa6c4cdaf969
luismeier95 <luis.meier95@googlemail.com>
2026-09-20 12:26:06 +0200

    Fix V15.1.52 static build version consistency

--- public/assets/js/00-theme-contract.js
+++ public/assets/js/00-theme-contract.js
@@ -1,7 +1,7 @@
 (()=>{
 'use strict';
 
-const VERSION=window.SKIELSEN_VERSION||'15.1.51';
+const VERSION=window.SKIELSEN_VERSION||'15.1.52';
 
 const TOKEN_MAP=Object.freeze({
   root_canvas:'--theme-root-canvas',
