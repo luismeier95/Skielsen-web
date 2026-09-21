e28cfed315654148065668998e340700d8a42724
luismeier95 <luis.meier95@googlemail.com>
2026-09-21 16:02:07 +0200

    Add reversible rounded HOME container experiment

--- public/assets/js/00-version.js
+++ public/assets/js/00-version.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const VERSION='15.1.84';
+const VERSION='15.1.85';
 window.SKIELSEN_VERSION=VERSION;
 function syncVersion(root=document){
   document.title=`SKIELSEN V${VERSION}`;
