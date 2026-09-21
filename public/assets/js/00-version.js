108dc36d6c226cf9cb05f34248e57a00135b5bb0
luismeier95 <luis.meier95@googlemail.com>
2026-09-21 20:11:01 +0200

    V15.1.101 widen desktop Admin Command title field

--- public/assets/js/00-version.js
+++ public/assets/js/00-version.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const VERSION='15.1.100';
+const VERSION='15.1.101';
 window.SKIELSEN_VERSION=VERSION;
 function syncVersion(root=document){
   document.title=`SKIELSEN V${VERSION}`;
