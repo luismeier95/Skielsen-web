31d8a8a708de78850ab4c8886feefbfcb58413b0
luismeier95 <luis.meier95@googlemail.com>
2026-09-21 16:05:51 +0200

    Expand HOME to 12px viewport margins

--- public/assets/js/00-version.js
+++ public/assets/js/00-version.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const VERSION='15.1.85';
+const VERSION='15.1.86';
 window.SKIELSEN_VERSION=VERSION;
 function syncVersion(root=document){
   document.title=`SKIELSEN V${VERSION}`;
