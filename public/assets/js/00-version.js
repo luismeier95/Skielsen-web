b628f908e1f5ad74e67c7df9acd0fe73937a4604
luismeier95 <luis.meier95@googlemail.com>
2026-09-21 21:00:40 +0200

    V15.1.106 add normalized mobile navigation icons

--- public/assets/js/00-version.js
+++ public/assets/js/00-version.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const VERSION='15.1.105';
+const VERSION='15.1.106';
 window.SKIELSEN_VERSION=VERSION;
 function syncVersion(root=document){
   document.title=`SKIELSEN V${VERSION}`;
--- public/assets/js/13-admin-command.js
+++ public/assets/js/13-admin-command.js
@@ -1,7 +1,7 @@
 (()=>{
 'use strict';
 
-const VERSION=window.SKIELSEN_VERSION||'15.1.105';
+const VERSION=window.SKIELSEN_VERSION||'15.1.106';
 const HOLD_MS=2000;
 let difficulty='NORMAL';
 let holdButton=null,holdStarted=0,holdRaf=0,lastSignature='',collapseRaf=0,mobileCollapsed=true;
