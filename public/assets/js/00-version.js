0ee8ec2010a090503b876ab7995766e758a6521e
luismeier95 <luis.meier95@googlemail.com>
2026-09-21 11:20:29 +0200

    Release SKIELSEN V15.1.79

--- public/assets/js/00-version.js
+++ public/assets/js/00-version.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const VERSION='15.1.78';
+const VERSION='15.1.79';
 window.SKIELSEN_VERSION=VERSION;
 function syncVersion(root=document){
   document.title=`SKIELSEN V${VERSION}`;
