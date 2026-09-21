2610271e7da740f1ca32794341957b3072f0a6ba
luismeier95 <luis.meier95@googlemail.com>
2026-09-21 21:07:32 +0200

    V15.1.108 reorder mobile HOME modules

--- public/assets/js/00-version.js
+++ public/assets/js/00-version.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const VERSION='15.1.107';
+const VERSION='15.1.108';
 window.SKIELSEN_VERSION=VERSION;
 function syncVersion(root=document){
   document.title=`SKIELSEN V${VERSION}`;
