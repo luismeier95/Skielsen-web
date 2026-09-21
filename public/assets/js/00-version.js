6c73ab4ea7b15cb89e5a2e3676c23dabf1b21d3a
luismeier95 <luis.meier95@googlemail.com>
2026-09-21 19:10:29 +0200

    V15.1.99 tighten desktop Admin Command layout

--- public/assets/js/00-version.js
+++ public/assets/js/00-version.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const VERSION='15.1.98';
+const VERSION='15.1.99';
 window.SKIELSEN_VERSION=VERSION;
 function syncVersion(root=document){
   document.title=`SKIELSEN V${VERSION}`;
