a9484d064a2cc4968ea2eff64e41707f23539ea2
luismeier95 <luis.meier95@googlemail.com>
2026-09-21 20:53:46 +0200

    V15.1.105 bust cache for themed MORE sheet

--- public/assets/js/00-version.js
+++ public/assets/js/00-version.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const VERSION='15.1.104';
+const VERSION='15.1.105';
 window.SKIELSEN_VERSION=VERSION;
 function syncVersion(root=document){
   document.title=`SKIELSEN V${VERSION}`;
