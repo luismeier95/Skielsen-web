6ef55cbde7e8631eb7c0e2eedd95153bd784ead3
luismeier95 <luis.meier95@googlemail.com>
2026-09-21 16:09:21 +0200

    Align fixed admin commands with HOME viewport margins

--- public/assets/js/00-version.js
+++ public/assets/js/00-version.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const VERSION='15.1.86';
+const VERSION='15.1.87';
 window.SKIELSEN_VERSION=VERSION;
 function syncVersion(root=document){
   document.title=`SKIELSEN V${VERSION}`;
