ddc165cb082f4cf3434996d80a50b9866e7df0c7
luismeier95 <luis.meier95@googlemail.com>
2026-09-21 20:23:57 +0200

    V15.1.103 swap mobile collapsed Admin Command fields

--- public/assets/js/00-version.js
+++ public/assets/js/00-version.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const VERSION='15.1.102';
+const VERSION='15.1.103';
 window.SKIELSEN_VERSION=VERSION;
 function syncVersion(root=document){
   document.title=`SKIELSEN V${VERSION}`;
