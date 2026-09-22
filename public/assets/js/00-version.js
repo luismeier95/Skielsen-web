70e6ecd45f80060af6cbdd9ddf58d1bcb641eddc
luismeier95 <luis.meier95@googlemail.com>
2026-09-23 00:48:02 +0200

    Bump SKIELSEN to V15.1.135

--- public/assets/js/00-version.js
+++ public/assets/js/00-version.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const VERSION='15.1.134';
+const VERSION='15.1.135';
 window.SKIELSEN_VERSION=VERSION;
 function syncVersion(root=document){
   document.title=`SKIELSEN V${VERSION}`;
