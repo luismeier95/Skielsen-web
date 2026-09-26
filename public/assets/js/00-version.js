fd1e5431d134b9c87a119e3162dbca42f91cd82e
Luis Meier <luis.meier95@googlemail.com>
2026-09-26 14:13:39 +0200

    style: round account landing page

--- public/assets/js/00-version.js
+++ public/assets/js/00-version.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-const VERSION='15.1.139';
+const VERSION='15.1.140';
 window.SKIELSEN_VERSION=VERSION;
 function syncVersion(root=document){
   document.title=`SKIELSEN V${VERSION}`;
