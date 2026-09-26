42219f3000e484f2be40b0556b11db52977bb3e4
luismeier95 <luis.meier95@googlemail.com>
2026-09-26 19:14:56 +0200

    Render oldest Disappear marks as original-shape outline

--- public/tic-tac-toe-test/app.js
+++ public/tic-tac-toe-test/app.js
@@ -99,14 +99,14 @@ function ageClass(symbol,index){
 function markSvg(symbol){
   if(symbol==='X'){
     return '<svg class="tttx-mark" viewBox="0 0 100 100" aria-hidden="true">'
-      +'<path class="tttx-shape" d="M22 22 L78 78 M78 22 L22 78"/>'
-      +'<path class="tttx-oldest-shape tttx-oldest-x" d="M18 18 L82 82 M82 18 L18 82"/>'
+      +'<path class="tttx-shape-fill" d="M14 22 L22 14 L50 42 L78 14 L86 22 L58 50 L86 78 L78 86 L50 58 L22 86 L14 78 L42 50 Z"/>'
+      +'<path class="tttx-shape-outline" d="M14 22 L22 14 L50 42 L78 14 L86 22 L58 50 L86 78 L78 86 L50 58 L22 86 L14 78 L42 50 Z"/>'
       +'</svg>';
   }
   return '<svg class="tttx-mark" viewBox="0 0 100 100" aria-hidden="true">'
-    +'<circle class="tttx-shape" cx="50" cy="50" r="31"/>'
-    +'<circle class="tttx-oldest-shape tttx-oldest-ring-outer" cx="50" cy="50" r="34"/>'
-    +'<circle class="tttx-oldest-shape tttx-oldest-ring-inner" cx="50" cy="50" r="22"/>'
+    +'<path class="tttx-shape-fill" fill-rule="evenodd" d="M50 12a38 38 0 1 1 0 76 38 38 0 0 1 0-76Zm0 14a24 24 0 1 0 0 48 24 24 0 0 0 0-48Z"/>'
+    +'<circle class="tttx-shape-outline" cx="50" cy="50" r="38"/>'
+    +'<circle class="tttx-shape-outline" cx="50" cy="50" r="24"/>'
     +'</svg>';
 }
 function renderBoard(){
